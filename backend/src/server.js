ea6c12e1eb4f3d03a73ec39f9e1a8f45e95c2220
Shreyash Patil <shreyashpatil2605@gmail.com>
2026-02-03 10:58:16 +0530

    Added some code for the vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,12 +29,19 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log("Server is running on port", ENV.PORT),
-    );
+    //Listen for local development 
+    if(ENV.NODE_ENV !== "production"){
+      app.listen(ENV.PORT, ()=> console.log("Server is up and  running on PORT:",ENV.PORT))
+    }
+
+    
   } catch (error) {
     console.error("Failed to start the server", error.message);
     process.exit(1);
   }
 };
 startServer();
+
+//export for the vercel
+export default app;
+
