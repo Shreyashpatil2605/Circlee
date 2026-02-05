454d7d66b406bf7ae230124e21866e70bb7b0702
Shreyash Patil <shreyashpatil2605@gmail.com>
2026-02-05 11:17:11 +0530

    Auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,16 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack/>
-    
-  )
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen
+          name="(auth)"
+          options={{ headerShown: false }}/>
+      </Stack>
+    </ClerkProvider>
+  );
 }
