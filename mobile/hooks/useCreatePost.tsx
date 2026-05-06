0814d0bee370c023281ad0dcb4dc78fbda99f45d
Shreyash Patil <shreyashpatil2605@gmail.com>
2026-05-06 17:11:42 +0530

    Add PostComposer component and enhance HomeScreen layout; update dependencies

---
+++ mobile/hooks/useCreatePost.tsx
@@ -0,0 +1,101 @@
+import { useApiClient } from "@/utils/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export const useCreatePost = () => {
+  const [content, setContent] = useState("");
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const api = useApiClient();
+  const queryClient = useQueryClient();
+  const createPostMutation = useMutation({
+    mutationFn: async (postData: { content: string; imageUri?: string }) => {
+      const formdata = new FormData();
+
+      if (postData.content) formdata.append("content", postData.content);
+      if (postData.imageUri) {
+        const urlParts = postData.imageUri.split(".");
+        const fileType = urlParts[urlParts.length - 1].toLowerCase();
+        const mimeTypeMap: Record<string, string> = {
+          png: "image/png",
+          jpg: "image/jpeg",
+          jpeg: "image/jpeg",
+          gif: "image/gif",
+          webp: "image/webp",
+        };
+        const mimeType = mimeTypeMap[fileType] || "image/jpeg";
+        formdata.append("image", {
+          uri: postData.imageUri,
+          name: `image.${fileType}`,
+          type: mimeType,
+        } as any);
+      }
+      return api.post("/posts", formdata, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    },
+    onSuccess: () => {
+      setContent("");
+      setSelectedImage(null);
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      Alert.alert("Success", "Post created Successfully");
+    },
+    onError: (error:any) => {
+      Alert.alert("Error", "Failed to create post. Please try again");
+        console.log("Status:", error?.response?.status);
+  console.log("Data:", error?.response?.data);
+  console.log("Message:", error?.message);
+    },
+  });
+  const handleImagePicker = async (useCamera: boolean = false) => {
+    const permissionResult = useCamera
+      ? await ImagePicker.requestCameraPermissionsAsync()
+      : await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.status !== "granted") {
+      const source = useCamera ? "camera" : "photo library";
+      Alert.alert(
+        "Permission needed",
+        `Please grant permission to acess your ${source}`,
+      );
+      return;
+    }
+    const pickerOptions = {
+      allowsEditing: true,
+      aspect: [9, 16] as [number, number],
+      quality: 0.8,
+    };
+    const result = useCamera
+      ? await ImagePicker.launchCameraAsync(pickerOptions)
+      : await ImagePicker.launchImageLibraryAsync({
+          ...pickerOptions,
+          mediaTypes: ["images"],
+        });
+    if (!result.canceled) setSelectedImage(result.assets[0].uri);
+  };
+  const createPost = () => {
+    if (!content && !selectedImage) {
+      Alert.alert(
+        "Empty Post",
+        "Please write something or add an image before posting!",
+      );
+      return;
+    }
+    const postData: { content: string; imageUri?: string } = {
+      content: content.trim(),
+    };
+    if (selectedImage) postData.imageUri = selectedImage;
+    createPostMutation.mutate(postData);
+  };
+  return {
+    content,
+    setContent,
+    selectedImage,
+    isCreating: createPostMutation.isPending,
+    pickImageFromGallery: () => handleImagePicker(false),
+    takePhoto: () => handleImagePicker(true),
+    removeImage: () => setSelectedImage(null),
+    createPost,
+  };
+};
--- mobile/hooks/useUserSync.ts
+++ mobile/hooks/useUserSync.ts
@@ -14,6 +14,10 @@ export const useUserSync = () => {
       console.log("User Synced Successfully:", response.data.user),
     onError: (error: any) => {
       console.log("User Synced failed: ", error);
+        console.log("Status:", error?.response?.status);
+  console.log("Data:", error?.response?.data);
+  console.log("Message:", error?.message);
+
     },
   });
   //AutoSync user When signed in
