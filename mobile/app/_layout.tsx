ff47af7856a7344dea942f34f108b7f21880396a
Shreyash Patil <shreyashpatil2605@gmail.com>
2026-05-25 19:16:58 +0530

    refactor: simplify RootLayout by removing ConvexProviderWithClerk and streamlining component structure

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,25 +1,33 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { View } from "react-native";
 import "../global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
+
 const queryclient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <QueryClientProvider client={queryclient}>
-          <View className="flex-1 bg-white">
-            <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#FFFFFF' } }}>
-              <Stack.Screen name="(auth)" />
-              <Stack.Screen name="(tabs)" />
-            </Stack>
-            <StatusBar style={"dark"} />
-          </View>
-        </QueryClientProvider>
-      </ConvexProviderWithClerk>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <QueryClientProvider client={queryclient}>
+        <View className="flex-1 bg-white">
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: { backgroundColor: "#FFFFFF" },
+            }}
+          >
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+          <StatusBar style={"dark"} />
+        </View>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
